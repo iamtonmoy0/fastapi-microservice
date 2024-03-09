64f6eafb59e0a03ca88361de42d5cfe704ed37d3
iamtonmoy0 <tonmoy2code@gmail.com>
2024-03-10 01:46:07 +0600

    Added .env and .venv to .gitignore. Added Redis connection and Product model to main.py.

--- Inventory/main.py
+++ Inventory/main.py
@@ -1,7 +1,28 @@
 from fastapi import FastAPI
+import os
+from dotenv import load_dotenv
+from redis_om import get_redis_connection, HashModel
 
 
+load_dotenv()
 app = FastAPI()
+
+redis = get_redis_connection(
+    host=os.getenv("REDIS_HOST"),
+    port=os.getenv("REDIS_PORT"),
+    password=os.getenv("REDIS_PASSWORD"),
+    decode_responses=True,
+)
+
+
+class Product(HashModel):
+    name: str
+    price: float
+    description: str
+    quantity: int
+
+    class Meta:
+        database = redis
 
 
 @app.get("/")
